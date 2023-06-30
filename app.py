8e9a659c0f97eb2bcd55337d728ec89de0ada3a6
alexxx77744 <alexxx7774@gmail.com>
2023-07-01 00:55:55 +0300

    Update app.py

--- app.py
+++ app.py
@@ -1,1 +1,19 @@
 
+python
+from flask import Flask, render_template, request
+from our_parser import parse_data
+
+app = Flask(__name__)
+
+@app.route('/')
+def index():
+    return render_template('index.html')
+
+@app.route('/parse', methods=['POST'])
+def parse():
+    url = request.form.get('url')
+    data = parse_data(url)
+    return render_template('data.html', data=data)
+
+if __name__ == '__main__':
+    app.run(debug=True)
